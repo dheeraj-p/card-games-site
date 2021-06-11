767a1510e3f7fbcedf1187adb3e1dbc0470502c9
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2021-06-11 21:41:14 +0530

    Dheeraj | Config to deploy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
+import Head from 'next/head';
 import '../global_styles/style.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Free Card Games</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
